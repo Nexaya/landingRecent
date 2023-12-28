41450ed73e50049d832a6b62fbb7348324c4a1b7
Nexaya <143490835+Nexaya@users.noreply.github.com>
2023-12-29 00:54:14 +0100

    Update Features.js
    changed Text

--- components/HomeOne/Features.js
+++ components/HomeOne/Features.js
@@ -7,8 +7,8 @@ const Features = () => {
             <div className="row">
                 <div className="col-lg-4 col-sm-6">
                     <div className="single-features">
-                        <h3><i className="bx bx-check-shield"></i> Security</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                        <h3><i className="bx bx-check-shield"></i>Global Growth Support</h3>
+                        <p>nexaya fuels the growth of businesses and startups worldwide. Our global team blends international knowledge and local insights to provide hands-on support, emphasizing Digital Transformation, Cyber Security, and Technology Strategy for robust growth.</p>
                         <span className="bx bx-check-shield"></span>
                     </div>
                 </div>
@@ -23,8 +23,8 @@ const Features = () => {
 
                 <div className="col-lg-4 col-sm-6 offset-sm-3 offset-lg-0">
                     <div className="single-features mb-0">
-                        <h3><i className="bx bx-certification"></i> Industry Certified</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                        <h3><i className="bx bx-certification"></i>Local Impact, Global Expertise</h3>
+                        <p>nexaya's Technology and Innovation Hubs in Europe and Africa serve as vital centers. They support companies in diverse IT and Technology areas, from Digital Transformation to Cyber Security. Explore local impact with global expertise at nexaya.</p>
                         <span className="bx bx-certification"></span>
                     </div>
                 </div>
