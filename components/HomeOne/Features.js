30e4eca1589b9cefcaba74c214a9fd45c6a50f29
Nexaya <143490835+Nexaya@users.noreply.github.com>
2023-12-29 01:08:36 +0100

    Update Features.js
    changed text

--- components/HomeOne/Features.js
+++ components/HomeOne/Features.js
@@ -8,7 +8,7 @@ const Features = () => {
                 <div className="col-lg-4 col-sm-6">
                     <div className="single-features">
                         <h3><i className="bx bx-check-shield"></i>Global Growth Support</h3>
-                        <p>nexaya fuels the growth of businesses and startups worldwide. Our global team blends international knowledge and local insights to provide hands-on support, emphasizing Digital Transformation, Cyber Security, and Technology Strategy for robust growth.</p>
+                        <p>nexaya fuels the growth of businesses and startups through international knowledge and local insights to provide hands-on support, emphasizing Digital Transformation, Cyber Security, and Technology Strategy for robust growth.</p>
                         <span className="bx bx-check-shield"></span>
                     </div>
                 </div>
