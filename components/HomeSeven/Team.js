e269841474ad2387bc975380dfc38d646662e585
Nexaya <143490835+Nexaya@users.noreply.github.com>
2024-01-05 22:08:34 +0100

    Update Team.js

--- components/HomeSeven/Team.js
+++ components/HomeSeven/Team.js
@@ -9,8 +9,9 @@ const Team = () => {
                     <div className="row align-items-center">
                         <div className="col-lg-6 col-md-12">
                             <div className="expert-team-content">
-                                <h3>Meet Our Expert Team Who are Behind The Whole Security Management</h3>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit adipiscing eleifend dictum potenti mattis viverra eget quam lacus enim porttitor bibendum elit dui nisl.</p>
+                                <h3>We are nexaya Global! Meet Our Team Who Are driving force behind our Whole Activities/h3>
+                                <p>Our team is known by its passion for pushing boundaries, 
+                                    a commitment to excellence, and an open-minded approach to exploring new ideas. We embrace creativity and a forward-thinking mindset </p>
 
                                 <Link href="/team">
                                     <a className="default-btn">View All Team Members</a>
