2892fe75e19da369d0c6f4a2d9375c3efb078291
ogagaCodes <ogagaadjaro@gmail.com>
2024-01-05 13:00:08 +0100

    Feat-Added Contact Form Layout

--- components/Contact/ContactInfo.js
+++ components/Contact/ContactInfo.js
@@ -1,45 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const ContactInfo = () => {
-    return (
-        <div className="contact-info-area">
-			<div className="container-fluid">
-				<div className="row">
-					<div className="col-lg-3 p-0">
-						<div className="single-contact-info">
-							<i className="bx bx-location-plus"></i>
-							<h3>New York</h3>
-							<p>88 Flower Avenue. Kingdom St. New York</p>
-							<a href="mailto:hello@pisa.com">Email: info@pisa.com</a>
-							<a href="tel:+822456974">+822456974</a>
-						</div>
-					</div>
-
-					<div className="col-lg-3 p-0">
-						<div className="single-contact-map">
-							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.1583091352!2d-74.11976373946234!3d40.69766374859258!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1588019781257!5m2!1sen!2sbd"></iframe>
-						</div>
-					</div>
-
-					<div className="col-lg-3 p-0">
-						<div className="single-contact-info">
-							<i className="bx bx-location-plus"></i>
-							<h3>California</h3>
-							<p>658 Lane Drive st Riverside. California</p>
-							<a href="mailto:hello@pisa.com">Email: hello@pisa.com</a>
-							<a href="tel:+892-569-756">+892-569-756</a>
-						</div>
-					</div>
-
-					<div className="col-lg-3 p-0">
-						<div className="single-contact-map">
-							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6509156.5834578!2d-123.79616103953882!3d37.1931243309143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb9fe5f285e3d%3A0x8b5109a227086f55!2sCalifornia%2C%20USA!5e0!3m2!1sen!2sbd!4v1591218371520!5m2!1sen!2sbd"></iframe>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-    )
-}
+  return (
+    <div className="container">
+      <div className="contact-info-area">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-6 p-0">
+              <div className="single-contact-info">
+                <i className="bx bx-location-plus"></i>
+                <h3>Germany</h3>
+                <p>FrankFurt Germany</p>
+                <a href="mailto:hello@nexaya.com">Email: hello@nexaya.com</a>
+                <a href="tel:+892-569-756">+892-569-756</a>
+              </div>
+            </div>
+            <div className="col-lg-6 p-0">
+              <div className="single-contact-map">
+                {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6509156.5834578!2d-123.79616103953882!3d37.1931243309143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb9fe5f285e3d%3A0x8b5109a227086f55!2sCalifornia%2C%20USA!5e0!3m2!1sen!2sbd!4v1591218371520!5m2!1sen!2sbd"></iframe> */}
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d327441.3481962601!2d8.306937234081005!3d50.12118576528076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bd096f477096c5%3A0x422435029b0c600!2sFrankfurt%2C%20Germany!5e0!3m2!1sen!2sbd!4v1704455851105!5m2!1sen!2sbd" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ContactInfo;
--- pages/contact.js
+++ pages/contact.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import Navbar from '../components/_App/Navbar';
-import PageBanner from '../components/Common/PageBanner';
-import FaqStyleOne from '../components/Common/FaqStyleOne';
-import FaqForm from '../components/Faq/FaqForm';
-import FooterThree from '../components/_App/FooterThree';
+import React from "react";
+import Navbar from "../components/_App/Navbar";
+import PageBanner from "../components/Common/PageBanner";
+import ContactInfo from "../components/Contact/ContactInfo";
+import FaqStyleOne from "../components/Common/FaqStyleOne";
+import FaqForm from "../components/Faq/FaqForm";
+import FooterThree from "../components/_App/FooterThree";
 
 const Contact = () => {
-    return (
-        <>
-            <Navbar />
+  return (
+    <>
+      <Navbar />
 
-            <PageBanner 
-                pageTitle="Contact Us" 
-                homePageUrl="/" 
-                homePageText="Home" 
-                activePageText="Contact Us" 
-            /> 
+      <PageBanner
+        pageTitle="Contact Us"
+        homePageUrl="/"
+        homePageText="Home"
+        activePageText="Contact Us"
+      />
 
-            <FaqStyleOne />
+      <FaqStyleOne />
+      <ContactInfo />
+      <FaqForm />
 
-            <FaqForm />
-            
-            <FooterThree />
-        </>
-    )
-}
+      <FooterThree />
+    </>
+  );
+};
 
 export default Contact;
